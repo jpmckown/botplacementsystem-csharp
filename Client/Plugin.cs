8cd7f40c6d7237aa45f7bcff55aa6498357c3c3a
acidphantasm <acidphantasm@nobody.com>
2026-08-10 14:33:19 -0500

    ver

--- Client/Plugin.cs
+++ Client/Plugin.cs
@@ -5,7 +5,7 @@ using EFT;
 
 namespace BotPlacementSystemClient;
 
-[BepInPlugin("com.acidphantasm.botplacementsystem", "acidphantasm-botplacementsystem", "2.0.19")]
+[BepInPlugin("com.acidphantasm.botplacementsystem", "acidphantasm-botplacementsystem", "2.1.0")]
 [BepInDependency("com.fika.headless", BepInDependency.DependencyFlags.SoftDependency)]
 public class Plugin : BaseUnityPlugin
 {
